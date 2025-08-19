6d6a389db6f1a32d9b4eb431e4f41fd106e8e81f
Hoang Cong Ly <congglyyy@gmail.com>
2025-08-19 23:57:06 +0700

    chapter04

---
+++ 24120378_hcmus/chapter04/Rectangle.h
@@ -0,0 +1,18 @@
+#ifndef _Rectangle_h
+#define _Rectangle_h
+
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+protected:
+    float width, height;
+public:
+    Rectangle(float width=0, float height=0);
+    float Area();
+    virtual void Input(istream& inDevice);
+};
+
+
+#endif
